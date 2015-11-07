660f903a0987848f3803e2372d69f3a259528f22
kenzk <kenashkraj@gmail.com>
2015-11-07 23:53:31 +0530

    Add common utils to the framework Add logger to the framework

---
+++ vylibs/Loglib/logger.py
@@ -0,0 +1,28 @@
+import logging
+
+class VyLog:
+    def __init__(self, handler='', default_level=''):
+        """
+        Initialise the PyVyuham logger module
+        """
+        self.handler = handler
+        
+    def log_info(self):
+        """
+        Log the information messages
+        """
+        
+    def log_warn(self):
+        """
+        Log the warning messages
+        """
+        
+    def log_debug(self):
+        """
+        Log the Debug messages
+        """
+        
+    def log_warn(self):
+        """
+        Log the warning messages
+        """
