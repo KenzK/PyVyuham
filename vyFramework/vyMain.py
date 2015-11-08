f1a61ed744c7e5ef3eb686df505c5d7b2e7c5c40
kenzk <kenashkraj@gmail.com>
2015-11-08 23:22:47 +0530

    Update

--- vyFramework/vyMain.py
+++ vyFramework/vyMain.py
@@ -1,8 +1,12 @@
 #Main object class for the Framework
+import vylogger
+
+import vyUtils.vymarshal as vymarshal
+import vyUtils.vyscout as vyscout
 
 class vyMain:
     def __init__(self):
         """Build PyVyuham
         """
-        self.scout=scout
-        self.marshal=marshal
+        self.scout=vyscout()
+        self.marshal=vymarshal()
